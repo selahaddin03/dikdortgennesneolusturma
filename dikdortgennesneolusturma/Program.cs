2c9927b268fcbb473eaf4768a4d8e37c5529625a
Selahaddin Doğan <149062529+selahaddin909@users.noreply.github.com>
2023-10-31 12:03:57 +0300

    Proje dosyası ekle.

---
+++ dikdortgennesneolusturma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dikdortgennesneolusturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(12, 2);
+            Console.WriteLine("Dikdörtgenin alanı=" + dikdortgen.alanHesapla());
+            Console.WriteLine("Dikdörtgenin alanı=" + dikdortgen.cevreHesapla());
+        }
+        class Dikdortgen
+        {
+            private uint kisaKenar, uzunKenar;
+            public Dikdortgen(uint kisaKenar, uint uzunKenar)
+            {
+                this.kisaKenar = kisaKenar;
+                this.uzunKenar = uzunKenar;
+            }
+            public uint alanHesapla()
+            {
+                return kisaKenar * uzunKenar;
+            }
+            public uint cevreHesapla()
+            {
+                return 2 * (kisaKenar + uzunKenar);
+            }
+        }
+    }
+}
+
